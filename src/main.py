3fb59ba9fbf0e6f81698b94a2bd405fe2c94cd8e
Clinten <70732411+DevWalrus@users.noreply.github.com>
2022-04-24 11:12:14 -0400

    Update docstrings to refect minor release

--- src/main.py
+++ src/main.py
@@ -12,7 +12,7 @@ Last Updated:
 Description:
     Created a version of the MDRC Bot to allow for angry emotions in addition to the pre-existing idle and wave actions
 Version:
-    2.0
+    2.0.1
 """
 
 from gpiozero import Servo, MotionSensor, LED, TonalBuzzer
